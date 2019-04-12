9e672902dd9d74838f8203b56e997d529711bf58
Aatoken <3410760440@qq.com>
2019-04-12 17:00:56 +0800

    项目创建，1.Application创建 2.添加AndroidX

---
+++ example/src/main/java/com/imooc/audition/activities/ExampleActivity.java
@@ -0,0 +1,23 @@
+package com.imooc.audition.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.imooc.audition.R;
+
+/**
+ * Author Aatoken
+ * Date 2019/4/12 16:54
+ * Description
+ */
+public class ExampleActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ example/src/main/java/com/imooc/audition/application/ExampleApp.java
@@ -0,0 +1,12 @@
+package com.imooc.audition.application;
+
+import android.app.Application;
+
+
+/**
+ * Author Aatoken
+ * Date 2019/4/12 16:55
+ * Description
+ */
+public class ExampleApp extends Application {
+}
