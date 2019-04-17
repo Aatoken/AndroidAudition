5ab4803454ff659f74b3922a79b1a869e7b96fc9
Aatoken <3410760440@qq.com>
2019-04-17 08:06:31 +0800

    修改忽略文件

--- example/src/main/java/com/imooc/audition/delegates/glide/GlideDelegate.java
+++ example/src/main/java/com/imooc/audition/delegates/glide/GlideDelegate.java
@@ -1,7 +1,7 @@
 package com.imooc.audition.delegates.glide;
 
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
+
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,11 +10,20 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.Request;
+import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.SizeReadyCallback;
+import com.bumptech.glide.request.target.Target;
+import com.bumptech.glide.request.transition.Transition;
 import com.imooc.audition.R;
 import com.imooc.latte_core.app.Latte;
 import com.imooc.latte_core.delegates.LatteDelegate;
 import com.imooc.latte_core.util.log.LatteLogger;
+import com.imooc.latte_core.util.toast.LatteToast;
 
 /**
  * Author Aatoken
@@ -29,7 +38,19 @@ public class GlideDelegate extends LatteDelegate implements View.OnClickListener
     private ImageView mImgview_show = null;
 
 
-
+    RequestOptions options = new RequestOptions()
+            .placeholder(R.mipmap.ic_launcher)//加载成功之前占位图
+            .error(R.mipmap.error)//加载错误之后的错误图
+            .override(400, 400)//指定图片的尺寸//指定图片的缩放类型为fitCenter （等比例缩放图片，宽或者是高等于ImageView的宽或者是高。）
+            .fitCenter()//指定图片的缩放类型为centerCrop （等比例缩放图片，直到图片的宽高都大于等于ImageView的宽度，然后截取中间的显示。）
+            .centerCrop()
+            .circleCrop()//指定图片的缩放类型为centerCrop （圆形）
+            .skipMemoryCache(true)//跳过内存缓存
+            .diskCacheStrategy(DiskCacheStrategy.ALL)//缓存所有版本的图像
+            .diskCacheStrategy(DiskCacheStrategy.NONE)//跳过磁盘缓存
+            .diskCacheStrategy(DiskCacheStrategy.DATA)//只缓存原来分辨率的图片
+            .diskCacheStrategy(DiskCacheStrategy.RESOURCE)//只缓存最终的图片
+            .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC);//智能选择
 
     @Override
     public Object setLayout() {
@@ -51,15 +72,33 @@ public class GlideDelegate extends LatteDelegate implements View.OnClickListener
         switch (v.getId()) {
             case R.id.img_loader:
 
-                String url = Latte.getApiHost()+"img.png";
-                Glide.with(this).load(url).into(mImgview_show);
+                String url = Latte.getApiHost() + "img.png";
+                Glide.with(this)
+                        .load(url)
+                        .apply(options)
+                        .listener(new RequestListener<Drawable>() {
+                            @Override
+                            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                                return false;
+                            }
 
-                LatteLogger.w("GlideDelegate","点击图片加载");
+                            @Override
+                            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+
+                                LatteToast.showToast("listener 成功！");
+                                return false;
+                            }
+                        })
+                        .into(mImgview_show);
+
+                LatteLogger.w("GlideDelegate", "点击图片加载");
                 break;
             default:
                 break;
         }
     }
+
+
 
 
 }
