f2045142f7169244680af2549b4dc76b1209cc7c
Aatoken <3410760440@qq.com>
2019-04-18 22:59:21 +0800

    在启动activity中消除actionbar

--- example/src/main/java/com/imooc/audition/activities/ExampleActivity.java
+++ example/src/main/java/com/imooc/audition/activities/ExampleActivity.java
@@ -1,5 +1,9 @@
 package com.imooc.audition.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+
 import com.imooc.audition.delegates.photoview.PhotoViewDelegate;
 import com.imooc.latte_core.activities.ProxyActivity;
 import com.imooc.latte_core.delegates.LatteDelegate;
@@ -20,5 +24,15 @@ public class ExampleActivity extends ProxyActivity {
     @Override
     public void post(Runnable runnable) {
 
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
     }
 }
